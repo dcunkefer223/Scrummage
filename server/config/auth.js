b1cea3a9d1c4fc9de57f3962b97f2130a2ebf9af
JParis44 <j.paris.caldwell@gmail.com>
2015-08-06 14:55:55 -0500

    Basic Database setup

---
+++ server/config/auth.js
@@ -0,0 +1,10 @@
+  module.exports = {
+    //environment variables for PostgreSQL
+    pgData : {
+      host: 'localhost',
+      database: 'postgres',
+      user : 'postgres',
+      port : 5432,
+      password : '',
+      ssl: false
+    }  };
