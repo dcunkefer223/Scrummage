8e2883b39fb49bfecb0e9e55a3b5f7bebeae063d
JParis44 <j.paris.caldwell@gmail.com>
2015-08-08 14:54:35 -0500

    Added addTask method

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,7 +1,21 @@
 var db = require('../db/db.js');
 
 module.exports.addTask = function (task) {
-	// task is {title, description, points, status, sprint_id, user_id}
+  // task is {title, description, points, status, sprint_id, user_id}
+  var testKeys = Object.keys(task).sort();
+  if (testKeys !== ['description', 'points', 'sprint_id', 'status', 'title', 'user_id']) {
+    return false;
+  }
+  db("features").insert(task).returning('id').then(
+    function (id) {
+      console.log('Task inserted at id: ' + id);
+      return true;
+    }, 
+    function (error) {
+      console.error(error);
+      return false;
+    }
+  );
 };
 
 module.exports.getAllTasks = function (sprint_id) {};
