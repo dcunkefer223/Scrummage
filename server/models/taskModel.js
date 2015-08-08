76463954cb0b27d83c6b4976e9e1d92bcde9665c
JParis44 <j.paris.caldwell@gmail.com>
2015-08-08 15:07:43 -0500

    Added getCompleteTasks method

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,7 +1,7 @@
 var db = require('../db/db.js');
 
 module.exports.addTask = function (task) {
-  // task is {title, description, points, status, sprint_id, user_id}
+  // task is {title, description, points, status[complete|inprogress|todo], sprint_id, user_id}
   var testKeys = Object.keys(task).sort();
   if (testKeys !== ['description', 'points', 'sprint_id', 'status', 'title', 'user_id']) {
     return false;
@@ -29,7 +29,16 @@ module.exports.getAllTasks = function (sprint_id) {
   );
 };
 
-module.exports.getCompleteTasks = function (sprint_id) {};
+module.exports.getCompleteTasks = function (sprint_id) {
+  db.select('*').from('features').where({sprint_id: sprint_id, status: 'complete'}).then(
+    function (tasks) {
+      return tasks;
+    }, 
+    function (error) {
+      console.error(error);
+    }
+  );
+};
 
 module.exports.getInProgressTasks = function (sprint_id) {};
 
