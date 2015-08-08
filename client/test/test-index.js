d29c92c86e44a824f85afc3ece555e841ccc446f
daniel-jansen <daniel.jansen88@gmail.com>
2015-08-08 11:57:34 -0500

    The Great Client Reorganization

--- client/test/test-index.js
+++ client/test/test-index.js
@@ -1,46 +1,20 @@
-angular.module('storyApp', ['ngDraggable'])
-    .controller('MainCtrl', function ($scope) {
-      var inArray = function (array, obj) {
-        var index = array.indexOf(obj);
+angular.module('storyBoard', ['dndLists'])
+    .controller('mainCtrl', function ($scope) {
+
+      $scope.models = {
+        selected: null,
+        lists: {"todo": [], "progress": [], "complete": []}
+      };
+
+      // Generate initial model
+      for (var i = 1; i <= 3; i++) {
+        $scope.models.lists.todo.push({label: "Task 1" + i});
+        $scope.models.lists.progress.push({label: "Task 2" + i});
       }
-      $scope.draggableObjects = [{name: 'Task One'}, {name: 'Task Two'}];
-      $scope.droppedObjects1 = [];
-      $scope.droppedObjects2 = [];
-      $scope.droppedObjects3 = [];
-      $scope.onDropComplete1 = function (data, event) {
-        var index = $scope.droppedObjects1.indexOf(data);
-        if (index == -1) {
-          $scope.droppedObjects1.push(data);
-        }
-      }
-      $scope.onDropComplete2 = function (data, event) {
-        var index = $scope.droppedObjects2.indexOf(data);
-        if (index == -1) {
-          $scope.droppedObjects2.push(data);
-        }
-      }
-      $scope.onDropComplete3 = function (data, event) {
-        var index = $scope.droppedObjects3.indexOf(data);
-        if (index == -1) {
-          $scope.droppedObjects3.push(data);
-        }
-      }
-      $scope.onDragSuccess1 = function(data, event) {
-        var index = $scope.droppedObjects1.indexOf(data);
-        if (index > -1) {
-          $scope.droppedObjects1.splice(index, 1);
-        }
-      }
-      $scope.onDragSuccess2 = function(data, event) {
-        var index = $scope.droppedObjects2.indexOf(data);
-        if (index > -1) {
-          $scope.droppedObjects2.splice(index, 1);
-        }
-      }
-      $scope.onDragSuccess3 = function(data, event) {
-        var index = $scope.droppedObjects3.indexOf(data);
-        if (index > -1) {
-          $scope.droppedObjects3.splice(index, 1);
-        }
-      }
+
+      // Model to JSON
+      $scope.$watch('models', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+      }, true);
+
     });
