68318a0c8c895246df4d4e070002b6e1a94c7f49
JParis44 <j.paris.caldwell@gmail.com>
2015-08-10 12:14:37 -0500

    Expanded basic taskHandler

--- server/requestHandlers/taskHandler.js
+++ server/requestHandlers/taskHandler.js
@@ -1,3 +1,18 @@
 var tasks = require('../models/taskModel.js');
 
-module.exports = {};
+module.exports.addFeature = function (feature) {
+	// feature is {title, description, points, status[complete|inprogress|todo], sprint_id, user_id}
+	tasks.addFeature(feature);
+};
+
+module.exports.getAllFeatures = function (sprint_id) {
+	tasks.getAllFeatures(sprint_id);
+};
+
+module.exports.getFeaturesByStatus = function (sprint_id, status) {
+	tasks.getFeaturesByStatus(sprint_id, status);
+};
+
+module.exports.getCommentsOnFeature = function (feature_id) {
+	tasks.getCommentsOnFeature(feature_id);
+};
