2bbc2b8b5080478cea8e06566b13a7581c21f62a
JParis44 <j.paris.caldwell@gmail.com>
2015-08-11 16:01:18 -0500

    Format fix and missed file for prev commit

--- server/requestHandlers/taskHandler.js
+++ server/requestHandlers/taskHandler.js
@@ -1,38 +1,38 @@
 var tasks = require('../models/taskModel.js');
 
 module.exports.addFeature = function (feature, res) {
-	// feature is {title, description, points, status[complete|inprogress|todo], sprint_id, team_id}
-	tasks.addFeature(feature, res);
+  // feature is {title, description, points, status[complete|inprogress|todo], sprint_id, team_id}
+  tasks.addFeature(feature, res);
 };
 
 module.exports.changeFeatureStatus = function (obj, res) {
-	tasks.changeFeatureStatus(obj.feature_id, obj.newStatus, res);
+  tasks.changeFeatureStatus(obj.feature_id, obj.newStatus, res);
 };
 
 module.exports.changeFeaturePoints = function (obj, res) {
-	tasks.changeFeaturePoints(obj.feature_id, obj.newPoints, res);
+  tasks.changeFeaturePoints(obj.feature_id, obj.newPoints, res);
 };
 
 module.exports.changeFeatureDescription = function (obj, res) {
-	tasks.changeFeatureDescription(obj.feature_id, obj.newDesc, res);
+  tasks.changeFeatureDescription(obj.feature_id, obj.newDesc, res);
 };
 
 module.exports.changeFeatureTitle = function (obj, res) {
-	tasks.changeFeatureTitle(obj.feature_id, obj.newTitle, res);
+  tasks.changeFeatureTitle(obj.feature_id, obj.newTitle, res);
 };
 
 module.exports.changeFeatureTeam = function (obj, res) {
-	tasks.changeFeatureTeam(obj.feature_id, obj.newTeam_id, res);
+  tasks.changeFeatureTeam(obj.feature_id, obj.newTeam_id, res);
 };
 
 module.exports.getAllFeatures = function (obj, res) {
-	tasks.getAllFeatures(obj.sprint_id, res);
+  tasks.getAllFeatures(obj.sprint_id, res);
 };
 
 module.exports.getFeaturesByStatus = function (obj, res) {
-	tasks.getFeaturesByStatus(obj.sprint_id, obj.status, res);
+  tasks.getFeaturesByStatus(obj.sprint_id, obj.status, res);
 };
 
 module.exports.getCommentsOnFeature = function (obj, res) {
-	tasks.getCommentsOnFeature(obj.feature_id, res);
+  tasks.getCommentsOnFeature(obj.feature_id, res);
 };
