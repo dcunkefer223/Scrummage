174248bd43b2389bd9a9a4240d190a63064b8b63
JParis44 <j.paris.caldwell@gmail.com>
2015-08-11 15:51:07 -0500

    Added changeUserTeam method to userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,6 +2,7 @@
 var db = require('../db/db.js');
 
 exports.addUser = function(user, cb){
+  // This method is for internal server use
   // user is {email, username, github_id}
   db('users').insert(user).returning('id').then(
     function (id) {
@@ -17,7 +18,7 @@ exports.addUser = function(user, cb){
 };
 
 exports.findUserByGithubId = function(id, cb){
-
+  // This method is for internal server use
   db.select('*').from('users').where('github_id', id).then(
     function (user){
       cb(null, user[0]);
@@ -30,3 +31,14 @@ exports.findUserByGithubId = function(id, cb){
 
 };
 
+module.exports.changeUserTeam = function (user_id, newTeam_id, res) {
+  db('users').where(id, user_id).update(team_id, newTeam_id).then(
+    function (rows) {
+      res.status(200).send({user_id: id});
+    }, 
+    function (error) {
+      console.error(error);
+      res.status(500).send('Failed to update feature in database');
+    }
+  );
+};
