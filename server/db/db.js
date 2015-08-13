b548ac1c886e29cca59d98df9d0023375eadb653
jmcgui05 <johnmac81@gmail.com>
2015-08-13 16:54:13 -0500

    db

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 
 var pg = require('pg');
-var auth = process.env.DATABASE_URL ? null : require('../config/authStore.js');
+var auth = require('../config/authStore.js');
 
 //This sets up you connection to the database. if on heroku, it links to the postgres database URL they give us.
 //Otherwise, it goes to the auth file and pulls it from there.
