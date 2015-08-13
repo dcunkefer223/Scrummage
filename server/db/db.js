50429134b5e838e40a8188d66ace7f060ca3752b
jmcgui05 <johnmac81@gmail.com>
2015-08-13 17:23:51 -0500

    authStore update

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 
 var pg = require('pg');
-var auth = process.env.DATABASE_URL ? null : require('../config/authStore.js');
+var auth = require('../config/authStore.js');
 
 //This sets up you connection to the database. if on heroku, it links to the postgres database URL they give us.
 //Otherwise, it goes to the auth file and pulls it from there.
@@ -14,7 +14,7 @@ var auth = process.env.DATABASE_URL ? null : require('../config/authStore.js');
 //   connection: process.env.DATABASE_URL || auth.pgData
 // });
 
-var db = new pg.Client(process.env.DATABASE_URL || auth.pgData);
+var db = new pg.Client(auth.pgData);
 db.connect();
 
 
