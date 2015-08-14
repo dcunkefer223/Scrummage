05442a4ac6e503a6794a6df516e0da7ce58df979
jmcgui05 <johnmac81@gmail.com>
2015-08-14 09:37:03 -0500

    fixed again

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 
 var pg = require('pg');
-var auth = process.env.DATABASE_URL ? null : require('../config/authStore.js');
+var auth = require('../config/authStore.js');
 
 //This sets up you connection to the database. if on heroku, it links to the postgres database URL they give us.
 //Otherwise, it goes to the auth file and pulls it from there.
@@ -9,13 +9,26 @@ var auth = process.env.DATABASE_URL ? null : require('../config/authStore.js');
 //Commenting out Paris settings to test
 //*************************************
 
+// var db = require('knex')({
+//   client: 'pg',
+//   connection: process.env.DATABASE_URL || auth.pgData
+// });
+
+//This works
+// var db = new pg.Client(auth.pgData);
+// db.connect();
+
 var db = require('knex')({
   client: 'pg',
-  connection: 'jansenfamily'
-});
-
-// var db = new pg.Client("");
-// db.connect();
+  connection: {
+    user: 'wtloxdvspmlazo',
+    password: 'NSQw_qVzfiy4hTSwK3MYTxMEaB',
+    host: 'ec2-54-83-55-214.compute-1.amazonaws.com',
+    port: 5432,
+    database: 'df8681a488cm7j',
+    ssl: true
+  }
+  });
 
 
 module.exports = db;
