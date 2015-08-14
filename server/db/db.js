729d06c65176c1b0d11834e43ac0f214cc73745a
jmcgui05 <johnmac81@gmail.com>
2015-08-14 09:38:59 -0500

    fixed again

--- server/db/db.js
+++ server/db/db.js
@@ -5,14 +5,13 @@ var auth = require('../config/authStore.js');
 //This sets up you connection to the database. if on heroku, it links to the postgres database URL they give us.
 //Otherwise, it goes to the auth file and pulls it from there.
 
-//*************************************
-//Commenting out Paris settings to test
-//*************************************
+
 
 // var db = require('knex')({
 //   client: 'pg',
 //   connection: process.env.DATABASE_URL || auth.pgData
 // });
+
 
 //This works
 // var db = new pg.Client(auth.pgData);
