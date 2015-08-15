0ed89bdfc14cb52e16faad6ae6b33837e762d875
Paris Caldwell <j.paris.caldwell@gmail.com>
2015-08-14 19:58:48 -0500

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -11,7 +11,7 @@ var auth = process.env.DATABASE_URL ? null : require('../config/authStore.js');
 
 var db = require('knex')({
   client: 'pg',
-  connection: 'jansenfamily'
+  connection: auth.pgData
 });
 
 // var db = new pg.Client("");
