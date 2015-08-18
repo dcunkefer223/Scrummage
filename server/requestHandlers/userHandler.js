45724c8aea063ede340b08b0f89d4cfcd102ecfe
daniel-jansen <daniel.jansen88@gmail.com>
2015-08-17 19:06:28 -0500

    Completed teams functionality

--- server/requestHandlers/userHandler.js
+++ server/requestHandlers/userHandler.js
@@ -10,12 +10,26 @@ module.exports.findUserByGithubId = function (user, cb) {
 
 module.exports.changeUserTeam = function (obj, user, res) {
   userModel.fetchTeamName(obj.name, res)
-  .then(function(newTeam_id) {
+  .then(function (newTeam_id) {
     return userModel.changeTeamId(user.id, newTeam_id[0].id);
   })
   .then(function (response) {
-    console.log('The response looks like', response);
     res.status(201).send({team_id: response[0]});
-  })
+  });
 
+};
+
+module.exports.createTeam = function (obj, user, res) {
+  var newPoints = JSON.stringify([0]);
+  var newName = obj.name;
+  userModel.createTeam({name: obj.name, backlog: newPoints, progress: newPoints, complete: newPoints}, res)
+  .then(function (newName) {
+    return userModel.fetchTeamName(obj.name);
+  })
+  .then(function (newTeam_id) {
+    return userModel.changeTeamId(user.id, newTeam_id[0].id);
+  })
+  .then(function (response) {
+    res.status(201).send({team_id: response[0]});
+  });
 };
