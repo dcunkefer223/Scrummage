f617a12d2309d347b738eaa0234ccb90c214d6e7
Paris Caldwell <j.paris.caldwell@gmail.com>
2015-08-27 16:57:05 -0500

    Updated db file

--- server/db/db.js
+++ server/db/db.js
@@ -5,28 +5,29 @@ var auth = require('../config/authStore.js');
 //This sets up you connection to the database. if on heroku, it links to the postgres database URL they give us.
 //Otherwise, it goes to the auth file and pulls it from there.
 
-
-// var db = require('knex')({
-//   client: 'pg',
-//   connection: process.env.DATABASE_URL || auth.pgData
-// });
+// LOCAL SETTINGS
+var db = require('knex')({
+  client: 'pg',
+  connection: auth.pgData
+});
 
 
 //This works
 // var db = new pg.Client(auth.pgData);
 // db.connect();
 
-var db = require('knex')({
-  client: 'pg',
-  connection: {
-    user: 'wtloxdvspmlazo',
-    password: 'NSQw_qVzfiy4hTSwK3MYTxMEaB',
-    host: 'ec2-54-83-55-214.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'df8681a488cm7j',
-    ssl: true
-  }
-  });
+// HEROKU SETTINGS
+// var db = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     user: 'wtloxdvspmlazo',
+//     password: 'NSQw_qVzfiy4hTSwK3MYTxMEaB',
+//     host: 'ec2-54-83-55-214.compute-1.amazonaws.com',
+//     port: 5432,
+//     database: 'df8681a488cm7j',
+//     ssl: true
+//   }
+//   });
 
 
 module.exports = db;
