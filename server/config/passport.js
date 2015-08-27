2ef013e4ee8af430a506fd9404e89c511eb7f272
Paris Caldwell <j.paris.caldwell@gmail.com>
2015-08-27 17:15:09 -0500

    Fixed passport file settings

--- server/config/passport.js
+++ server/config/passport.js
@@ -23,9 +23,9 @@ module.exports = function(passport) {
   //Github OAuth**
   //*************************************************************
   passport.use(new GithubStrategy({
-    clientID: '5057c4d093220875cc38',
-    clientSecret: 'cf79e078480f4dda85ec4946c10353e215d8a9db',
-    callbackURL: '127.0.0.1:3000/auth/github/callback',
+    clientID: '3cf6f618800a697e2bc5',
+    clientSecret: 'b160a044255899ac3cc086064b8783ff40fd0c23',
+    callbackURL: 'http://127.0.0.1:3000/auth/github/callback',
     passReqToCallback: true
   },
 
