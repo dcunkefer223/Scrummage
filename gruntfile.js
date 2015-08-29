8b27a5651eef73a964ffd189e49c1b4a808466f8
jmcgui05 <johnmac81@gmail.com>
2015-08-29 16:45:25 -0500

    configured grunt file to be more production ready

--- gruntfile.js
+++ gruntfile.js
@@ -2,53 +2,73 @@ module.exports = function (grunt){
   grunt.initConfig({
   pkg: grunt.file.readJSON('package.json'),
 
+  concat_css: {
+    // options: {
+    //   // Task-specific options go here. 
+    // },
+    all: {
+      src: ["client/css/*.css"],
+      dest: "build/production.css"
+    },
+  },
+
   cssmin: {
     target: {
       files: [{
         expand: true,
-        cwd: 'client/css/',
-        src: ['*.css'],
-        dest: 'client/css/',
+        cwd: 'build/', 
+        src: ['production.css'],
+        dest: 'build/',
         ext: '.min.css'
       }]
     }
   },
 
-  // imagemin: {
-  //   dynamic: {
-  //     files: [{
-  //       expand: true,
-  //       cwd: 'client/img/',
-  //       src: ['*.jpg'],
-  //       dest: 'client/img/'
-  //     }]
-  //   }
-  // },
 
-  concat: {
-    serverSide: {
-      src: [
-      'server/**/*.js',
-      ],
-      dest: 'server/build/production.js',
-    },
-    clientSide: {
-      src: [
-      'client/js/*.js',
-      ],
-      dest: 'client/build/production.js',
+  imagemin: {
+    dynamic: {
+      files: [{
+        expand: true,
+        cwd: 'client/img/',
+        src: ['*.{png,jpg,gif}'],
+        dest: 'build/'
+      }]
     }
   },
 
+  concat: {
+    options: {
+      separator: ';',
+    },
+    dist: {
+      src: ['server/**/*.js', 'client/js/*.js'],
+      dest: 'build/production.js',
+    },
+  },
+  // concat: {
+  //   serverSide: {
+  //     src: [
+  //     'server/**/*.js',
+  //     ],
+  //     dest: 'server/build/serverProduction.js',
+  //   },
+  //   clientSide: {
+  //     src: [
+  //     'client/js/*.js',
+  //     ],
+  //     dest: 'client/build/clientProduction.js',
+  //   }
+  // },
+
   uglify: {
     serverSide: {
-      src: 'server/build/production.js',
-      dest: 'server/build/production.min.js'
-    },
-    clientSide: {
-      src: 'client/build/production.js',
-      dest: 'client/build/production.min.js'
+      src: 'build/production.js',
+      dest: 'build/production.min.js'
     }
+    // clientSide: {
+    //   src: 'client/build/clientProduction.js',
+    //   dest: 'client/build/clientProduction.min.js'
+    // }
   },
 
 
@@ -75,11 +95,11 @@ module.exports = function (grunt){
 
   //Tell grunt to use these plugins
 
-
-  // grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-concat-css');
   grunt.loadNpmTasks('grunt-contrib-watch');
   // grunt.loadNpmTasks('grunt-contrib-jshint');
 
@@ -97,7 +117,7 @@ module.exports = function (grunt){
 });
 
   //what happens when you type grunt in CL
-  grunt.registerTask('default', ['cssmin', 'concat', 'uglify', 'watch']);
+  grunt.registerTask('default', ['concat_css', 'cssmin', 'imagemin', 'concat', 'uglify', 'watch']);
   // grunt.registerTask('default', ['imagemin']);
   grunt.registerTask('build', ['concat']);
   //grunt.registerTask('default', ['mochaTest']); will replace with Jasmine if possible
